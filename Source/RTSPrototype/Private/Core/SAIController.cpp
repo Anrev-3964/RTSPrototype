224221dffe1ed74438ae25b01ac21df7b98b956e
Endersuit <giovanniscurosu84ita@gmail.com>
2024-12-17 18:20:32 +0100

    FactionSystem0.3
    now the RTSPrototypeCharacter can see other Actors by using AIPercetion System set in SAIController

--- Source/RTSPrototype/Private/Core/SAIController.cpp
+++ Source/RTSPrototype/Private/Core/SAIController.cpp
@@ -3,17 +3,61 @@
 
 #include "Core/SAIController.h"
 
+#include "Perception/AIPerceptionComponent.h"
+
 //dovra ordinare al proprio pawn di muoveris verso la destinaizone usanod un navMEsh
+
+ASAIController::ASAIController()
+{
+	// Crea il componente di percezione
+	AIPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerceptionComponent"));
+	AIPerceptionStimuliSource = CreateDefaultSubobject<UAIPerceptionStimuliSourceComponent>(TEXT("AIPerceptionStimuliSource"));
+}
+
+void ASAIController::BeginPLay()
+{
+	Super::BeginPlay();
+	if (AIPerceptionComponent)
+	{
+		SightConfig = NewObject<UAISenseConfig_Sight>(this);
+		if (SightConfig)
+		{
+			//TO DO : prendere dirretamente i valori dal blueprint del pawn
+			SightConfig->SightRadius = 1000.f;           
+			SightConfig->LoseSightRadius = 1200.f;     
+			SightConfig->PeripheralVisionAngleDegrees = 180.f; 
+			SightConfig->AutoSuccessRangeFromLastSeenLocation = 150.f; 
+
+			// Configura il componente di percezione con il senso visivo
+			AIPerceptionComponent->ConfigureSense(*SightConfig);
+			PerceptionComponent->SetDominantSense(SightConfig->GetSenseImplementation());
+			AIPerceptionComponent->SetSenseEnabled(UAISense_Sight::StaticClass(), true);
+		}
+		AIPerceptionComponent->OnPerceptionUpdated.AddDynamic(this, &ASAIController::OnPerceptionUpdated);
+	}
+}
 
 void ASAIController::NavigateToDestination(const FVector& Destination)
 {
 	if (APawn* ControlledPawn = GetPawn())
 	{
 		// Muovere il pawn verso la destinazione con il navmesh
-		 MoveToLocation(Destination, -1.0f, true, true, false, true, nullptr, false);
+		MoveToLocation(Destination, -1.0f, true, true, false, true, nullptr, false);
 	}
 	else
 	{
-			UE_LOG(LogTemp, Warning, TEXT("this pawn doesent have an AICOntroller"));
+		UE_LOG(LogTemp, Warning, TEXT("this pawn doesent have an AICOntroller"));
+	}
+}
+
+void ASAIController::OnPerceptionUpdated(const TArray<AActor*>& UpdatedActors)
+{
+	for (AActor* Actor : UpdatedActors)
+	{
+		if (Actor)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Ho percepito un attore: %s"), *Actor->GetName());
+			// Puoi aggiungere logica per reagire a ciò che è stato percepito, come inseguire un nemico
+		}
 	}
 }
--- Source/RTSPrototype/Public/Core/SAIController.h
+++ Source/RTSPrototype/Public/Core/SAIController.h
@@ -4,17 +4,47 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AIPerceptionStimuliSourceComponent.h"
+#include "Perception/AISense_Sight.h"
+#include "Perception/AISenseConfig_Sight.h" 
 #include "SAIController.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class RTSPROTOTYPE_API ASAIController : public AAIController
 {
 	GENERATED_BODY()
 
-public :
+	public :
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadWrite)
+	UAIPerceptionComponent* AIPerceptionComponent;
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadWrite)
+	UAIPerceptionStimuliSourceComponent* AIPerceptionStimuliSource;
+	
+	//this values will by passed by units
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI Settings")
+	float UnitSightRadius;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI Settings")
+	float UnitLoseSightRadius;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI Settings")
+	float UnitPeripheralVisionAngleDegrees;
+	
+	ASAIController();
+	
+	void BeginPLay();
+
 	UFUNCTION()
 	void NavigateToDestination(const FVector& Destination);
+
+private:
+
+	UPROPERTY()
+	UAISenseConfig_Sight* SightConfig;
+	UFUNCTION()
+	void OnPerceptionUpdated(const TArray<AActor*>& UpdatedActors);
 };
+
